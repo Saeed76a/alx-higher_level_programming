3dab6e8557147641736885e0763171a7c4e8f08a
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-05 05:23:38 -0700

    task10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,19 +7,19 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *tmp;
+	listint_t *current, *tmp = list;
 
 	if (!list)
 		return (0);
 	current = list;
-	current = current->next;
-	tmp = current;
 	while (current && tmp)
 	{
 		current = current->next;
 		if (!current)
 			return (0);
 		tmp = tmp->next->next;
+		if (!tmp)
+			return (0);
 		if (tmp == current)
 			return (1);
 	}

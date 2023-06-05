98dcc49138b5b73b09ef7cb1352b61f435663064
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-05 03:34:08 -0700

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: a list of nodes
+ * Return: 0 if is safe
+ * 1 returned if is unsafe
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *tmp;
+
+	if (!list)
+		return (0);
+	current = list;
+	while(current)
+	{
+		tmp = list;
+		if (current == current->next)
+			return (1);
+		while (tmp != current)
+		{
+			if (current->next == tmp)
+				return (1);
+			tmp = tmp->next;
+		}
+		current = current->next
+	}
+	return (0);
+}

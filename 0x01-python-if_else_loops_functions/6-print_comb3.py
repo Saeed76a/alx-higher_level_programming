9def3f15627f6f02e6ccc5d2721c308c99db9811
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-06 15:09:10 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num1 in range(1, 9):
+for num1 in range(10):
     for num2 in range(num1 + 1, 10):
         if num2 < 9 or num1 < 8:
             print("{:d}".format(num1) + "{:d}".format(num2), end=", ")

93a06a225984b000a0fa519d7863ec06bd2283bc
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-11 09:37:15 +0100

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix[0]) != 0:
         for i in matrix:
             for j in i:
-                print("{}".format(j), end=' ' if j != i[len(i) - 1] else '\n')
+                print("{:d}".format(j), end=' ' if j != i[len(i) - 1] else '\n')
     else:
         print("".format())

26a8ef1dbe7129b8371dfaedd6afe6f4a249f2bc
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-11 10:55:07 +0100

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    a = 0
+    if len(my_list) > 0:
+        for i in my_list:
+            b = a > i
+            if b is False:
+                a = i
+    return a

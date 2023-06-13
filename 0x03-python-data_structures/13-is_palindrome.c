d20313b5fbe6bb3f8eaab58b2cda8bc2150b5c4d
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-13 03:44:06 +0100

    task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,33 +11,23 @@ int is_palindrome(listint_t **head)
     listint_t *self = *head, *mirror = *head, *prev = NULL, *nextNode = NULL;
     int length = 0, i = 0;
 
-   
     while (mirror && mirror->next)
     {
         length += 2;
         mirror = mirror->next->next;
     }
-
-    
     if (mirror != NULL)
         length++;
-
     mirror = *head;
-
-    
     for (i = 0; i < length / 2; i++)
         mirror = mirror->next;
-
-    
     while (mirror)
     {
         nextNode = mirror->next;
         mirror->next = prev;
         prev = mirror;
         mirror = nextNode;
-    }
-
-    
+    }    
     mirror = prev;
     while (mirror)
     {
@@ -46,6 +36,5 @@ int is_palindrome(listint_t **head)
         self = self->next;
         mirror = mirror->next;
     }
-
     return (1);
 }

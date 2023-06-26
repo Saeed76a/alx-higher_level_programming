6e05efa39bf16195bf3a1fd63846b878c141ca03
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-26 14:59:54 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	class outOfRange(Exception):
-		def __init__(self, *args, **kwargs):
-			Exception.__init__(self, *args, **kwargs)
-	try:
-		i = 0
-		for l in my_list:
-			print(l, end='')
-			i += 1
-			if i == x:
-				break
-		print()
-		if i != x:
-			raise outOfRange
-		else:
-			return x
-	except outOfRange:
-		return i
+    class outOfRange(Exception):
+        def __init__(self, *args, **kwargs):
+            Exception.__init__(self, *args, **kwargs)
+    try:
+        i = 0
+        for num in my_list:
+            print(num, end='')
+            i += 1
+            if i == x:
+                break
+        print()
+        if i != x:
+            raise outOfRange
+        else:
+            return x
+    except outOfRange:
+        return i

aa4ba9bd4d5f9392a1258d202dc9cbdf15e15c09
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-27 18:50:36 +0100

    last task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from math import pi
+class MagicClass:
+    def __init__(self, radius):
+        self.__radius = 0
+        if not isinstance(raduis, int) and not isinstance(raduis, float):
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+        return self.__radius
+    def area(self):
+        return (self.__radius **2 * pi)
+    def circumference(self):
+        return (2 * pi * self.__radius)

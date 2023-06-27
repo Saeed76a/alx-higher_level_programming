0bed0928d66c46bf2fdf3999a82393b3f6393ec1
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-27 19:05:27 +0100

    last task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
+"""define the Python class MagicClass that does exactly the same\
+      as the following Python bytecode:"""
 from math import pi
 
 
 class MagicClass:
+    """set a circle"""
+
     def __init__(self, radius):
+        """intialize MagicClass:
+        Args:
+            radius (float and int): the raduis of the circle
+        """
         self.__radius = 0
         if not isinstance(raduis, int) and not isinstance(raduis, float):
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
+        """Return the current radius"""
         return self.__radius
 
     def area(self):
+        """Return the area of the circle"""
         return (self.__radius ** 2 * pi)
 
     def circumference(self):
+        """Return the circumference of the circle"""
         return (2 * pi * self.__radius)

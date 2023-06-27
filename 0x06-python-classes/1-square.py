ff7fc8d2422d8e004d18a59e1573506e649c045b
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-27 16:46:10 +0100

    task 1 && task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """representing square operation"""
+    def ___init__(self, size)
+        """intialize the squre
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size

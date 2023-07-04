4a00bf2c12c5d6e0f6f390a10d5158c0dc32b4fd
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-04 14:01:09 +0100

    task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,10 +4,4 @@
 
 class LockedClass(object):
     """define a LockedClass class"""
-    def __setattr__(self, key, value):
-        """initialize a LockedClass class"""
-        if not key == "first_name":
-            raise AttributeError("'{}' object has no \
-                                 attribute '{}'\
-                                 ".format(self.__class__.__name__, key))
-        object.__setattr__(self, key, value)
+    __slots__ = ["first_name"]

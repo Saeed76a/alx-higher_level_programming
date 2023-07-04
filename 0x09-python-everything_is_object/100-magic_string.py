b710f6e11d494cbc1e0532b27d782345cd9ce48f
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-04 13:55:54 +0100

    task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-L = [0]
-def magic_string():
-    L[0] += 1
-    return (", ".join(["BestSchool" for i in range(L[0])]))
+def magic_string(L=[]):
+    L.append("BestSchool")
+    return ", ".join(L) 

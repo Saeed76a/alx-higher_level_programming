2bc241c03b121b0c5d1b0cd570515f37b4268cf2
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-12 00:10:26 +0100

    read file and print the contiant

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ 0. Read file
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8)
+    and prints it to stdout:
+
+    Args:
+        filename (str): tha name of the file to read
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data)

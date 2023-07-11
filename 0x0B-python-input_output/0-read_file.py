016a9eeba0d730835db460dc6d45df8deac46eb7
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-12 00:18:54 +0100

    read file and print the contiant

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,4 +15,4 @@ def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
     for line in read_data:
-        print(line, end = '')
+        print(line, end='')

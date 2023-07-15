66a2eb7ae4d7a6233844bb8cf293b2b02aac7485
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-15 18:28:03 +0100

    task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+9. Student to JSON
+"""
+
+
+class Student:
+    """class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the object with the provided arguments.
+
+        Args:
+            first_name (str): student's first name.
+            last_name (str): student's last name.
+            age (int): the age
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        get the dictionary description
+        with simple data structure
+
+        Args:
+            attrs (list): list of attributes
+        """
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for attr in attrs:
+            try:
+                my_dict[attr] = self.__dict__[attr]
+            except:
+                return self.__dict__
+        return my_dict

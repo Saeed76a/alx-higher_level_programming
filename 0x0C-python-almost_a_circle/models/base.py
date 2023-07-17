722729defdc42653a507ca3f59e3d06117b92af5
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-17 23:12:17 +0100

    fix docstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,30 +2,18 @@
 import json
 """
 The Base class serves as the base class
-for other classes in the project.
 """
 
 
 class Base:
     """
     This class will be the “base” of
-    all other classes in this project.
-
-    description:
-        manage id attribute in all
-        my future classes
-    class attribute:
-        private:
-            __nb_objects (int)
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
         Initialize the object with the provided arguments.
-
-        Args:
-            id (int): attribute's id
         """
         if id is not None:
             self.id = id

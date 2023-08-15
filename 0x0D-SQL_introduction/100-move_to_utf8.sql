4e5a70f27fca45b05e68f6199a6d65006ff8096a
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-16 00:46:43 +0100

    17.2

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- Go to UTF8
 USE hbtn_0c_0; ALTER DATABASE hbtn_0c_0  
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT 
+ALTER TABLE hbtn_0c_0.first_table CONVERT 
 TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) 
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) 
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

3257d0d071652448841c49d8a013a2c163b9508f
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-17 02:15:22 +0100

    citessds

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- Cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id INT AUTO-INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT FOREIGN KEY NOT NULL,
-    name VARCHAR(256) NOT NULL
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE(id), FOREIGN KEY(state_id) REFERENCES states(id)
+);

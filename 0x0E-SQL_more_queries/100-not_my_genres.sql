601d529bf3558e4374e0f06031aefc8f47e72eff
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-17 04:02:51 +0100

    100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,17 +1,18 @@
 -- all records
 SELECT DISTINCT name
-  FROM tv_genres AS g
-       INNER JOIN tv_show_genres
-       ON g.id = tv_show_genres.genre_id
-
-       INNER JOIN tv_shows AS t
-       ON tv_show_genres.show_id = t.id
-       WHERE g.name NOT IN
-             (SELECT name
-                FROM tv_genres AS g
-	             INNER JOIN tv_show_genres
-		     ON g.id = tv_show_genres.genre_id
-		INNER JOIN tv_shows AS t
-		ON tv_show_genres.show_id = t.id
-	WHERE t.title = "Dexter")
- ORDER BY g.name;
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name NOT IN
+(
+SELECT name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+)
+ORDER BY g.name;

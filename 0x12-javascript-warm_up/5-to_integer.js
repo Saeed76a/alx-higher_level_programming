3810ce9ae5930b5f8dc5f50f1718eef6f7e1b78c
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-11 16:51:51 +0100

    finally

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const argv = process.argv;
 
-if (isNaN(parseInt(argv[2])) == false){
-    let num = Number(argv[2]);
-    console.log('My number: ' + num);
-} else {console.log('Not a number');}
+if (isNaN(parseInt(argv[2])) === false) {
+  const num = Number(argv[2]);
+  console.log('My number: ' + num);
+} else { console.log('Not a number'); }

fafad5c3d4d484f7da8fe84ae9174c4bc2176577
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-16 14:56:47 +0100

    all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+    Mydb = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = Mydb.cursor()
+
+    cur.execute('SELECT * FROM cities ORDER BY id ASC')
+    all = cur.fetchall()
+
+    for one in all:
+        print(one)

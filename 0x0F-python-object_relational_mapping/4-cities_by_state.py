6fa064fc3b5c417de699966dafdac899ceff8435
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-16 15:16:19 +0100

    finally

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
     )
     cur = Mydb.cursor()
 
-    cur.execute('SELECT * FROM cities ORDER BY id ASC')
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.state_id ASC')
     all = cur.fetchall()
 
     for one in all:

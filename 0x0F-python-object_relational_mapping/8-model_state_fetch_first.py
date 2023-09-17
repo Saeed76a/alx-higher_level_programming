ab63f3c86f8986b82e430dd03b0cd9aaeb8568d1
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-17 14:29:32 +0100

    all

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
 '.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
-    all = session.query(State).order_by(State.id).all()
+    first = session.query(State).filter_by(id=1).first()
     try:
-        print(all[0].id, all[0].name, sep=': ')
-    except IndexError:
+        print(first.id, first.name, sep=': ')
+    except Exception:
         print("Nothing")

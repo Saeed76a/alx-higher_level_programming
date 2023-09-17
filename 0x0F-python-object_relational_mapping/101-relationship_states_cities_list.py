8bb9e8667e6addde9da17a3384746104d3273c47
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-17 22:36:09 +0100

    i did it hhhhh

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Get a state
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}\
+'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    all = session.query(State, City).filter(City.state_id == State.id).all()
+    dictionary = {}
+    for state in all:
+        dictionary['{}: {}\
+'.format(state[0].id, state[0].name)] = []
+        for __ in all:
+            if state[0].name == __[0].name:
+                dictionary['{}: {}\
+'.format(state[0].id, state[0].name)].append('\t{}: {}\
+'.format(__[1].id, __[1].name))
+    for state in dictionary:
+        print(state)
+        for city in dictionary[state]:
+            print(city)

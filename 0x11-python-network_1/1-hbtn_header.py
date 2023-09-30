679d3a6f171188e1acf5187460a326f6676ba83e
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-30 02:54:22 +0100

    requarement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,7 @@
 import urllib.request
 import sys
 
-
-with urllib.request.urlopen(sys.argv[1])\
-      as response:
-    html = response.read()
-    print(response.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.read()
+        print(response.headers.get('X-Request-Id'))

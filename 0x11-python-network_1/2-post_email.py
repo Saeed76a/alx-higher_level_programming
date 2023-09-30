68d7a26c6a35f291bc0ef36ee9bca2e764293073
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-30 03:08:28 +0100

    hard coding is a hard working, can you change my email balis

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
+"""hard coding is a hard working"""
 
-
-data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
-req = urllib.request.Request(sys.argv[1], data=data, method='POST')
-with urllib.request.urlopen(req) as response:
-    result = response.read()
-print(str(result)[2:-1])
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        result = response.read()
+    print(str(result)[2:-1])

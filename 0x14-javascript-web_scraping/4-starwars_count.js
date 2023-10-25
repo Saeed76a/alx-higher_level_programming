a5e93f566d600d1aee66da661a5e74d90f49ee25
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-25 02:00:31 +0100

    4ss

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,34 @@
 const request = require('request');
 
 const url = process.argv[2];
-let num = 0;
-request.get(url, (err, resp) => {
+// let num = 0;
+// request.get(url, (err, resp) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     const all = JSON.parse(resp.body).results;
+//     for (const res in all) {
+//       if (all[res].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+//         num++;
+//       }
+//     }
+//     console.log(num);
+//   }
+// });
+request(url, function (err, body) {
   if (err) {
     console.error(err);
   } else {
-    const all = JSON.parse(resp.body).results;
-    for (const res in all) {
-      if (all[res].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        num++;
-      }
-    }
-    console.log(num.toString());
+    let counter = 0;
+    const { results } = JSON.parse(body.body);
+
+    results.forEach((element, i) => {
+      element.characters.forEach((element, j) => {
+        if (element.includes('/18/')) {
+          counter++;
+        }
+      });
+    });
+    console.log(counter);
   }
 });
